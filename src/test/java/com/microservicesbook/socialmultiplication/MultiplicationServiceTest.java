b02e6cd6b378cf26ed7be7954d2774ef5fd24abe
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-08-23 18:01:32 -0500

    feat: set up of the implementation of the multiplication service and unit test but still fail since no random generation implementation yet.

---
+++ src/test/java/com/microservicesbook/socialmultiplication/MultiplicationServiceTest.java
@@ -0,0 +1,41 @@
+package com.microservicesbook.socialmultiplication;
+
+import com.microservicesbook.socialmultiplication.domain.Multiplication;
+import com.microservicesbook.socialmultiplication.service.MultiplicationService;
+import com.microservicesbook.socialmultiplication.service.RandomGeneratorService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+
+public class MultiplicationServiceTest {
+
+    @MockBean
+    private RandomGeneratorService randomGeneratorService;
+
+    @Autowired
+    private MultiplicationService multiplicationService;
+
+    @Test
+    public void createRandomMultiplicationTest() {
+
+        //given
+        given(randomGeneratorService.generateRandomFactor()).
+                willReturn(50, 30);
+
+        //when
+        Multiplication multiplication = multiplicationService.createRandomMultiplication();
+
+        //then
+        assertThat(multiplication.getFactorA()).isEqualTo(50);
+        assertThat(multiplication.getFactorB()).isEqualTo(30);
+        assertThat(multiplication.getResult()).isEqualTo(1500);
+
+    }
+
+}
