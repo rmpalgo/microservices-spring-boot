044205fe37c541c8135c4f50d0c6ce917d9afe04
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-08-23 17:19:55 -0500

    feat: setup multiplication class.

---
+++ src/main/java/Multiplication.java
@@ -0,0 +1,48 @@
+public class Multiplication {
+
+    private int factorA;
+    private int factorB;
+
+    private int result;
+
+    public Multiplication(int factorA, int factorB, int result) {
+        this.factorA = factorA;
+        this.factorB = factorB;
+        this.result = result;
+    }
+
+    public int getFactorA() {
+        return factorA;
+    }
+
+    public void setFactorA(int factorA) {
+        this.factorA = factorA;
+    }
+
+    public int getFactorB() {
+        return factorB;
+    }
+
+    public void setFactorB(int factorB) {
+        this.factorB = factorB;
+    }
+
+    public int getResult() {
+        return result;
+    }
+
+    public void setResult(int result) {
+        this.result = result;
+    }
+
+    @Override
+    public String toString() {
+        return "Multiplication{" +
+                "factorA=" + factorA +
+                ". factorB=" + factorB +
+                ", result(A*B)=" +
+                result +
+                '}';
+    }
+
+}
---
+++ src/test/java/com/microservicesbook/socialmultiplication/SocialMultiplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicesbook.socialmultiplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMultiplicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
